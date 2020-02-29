c659a95ada710888d9f66c75777027d725d12e5f
Pramod Kadam <43199778+pramod21kadam@users.noreply.github.com>
2020-03-01 01:23:23 +0530

    Update MoveFiles.py
    Used JSON files

--- MoveFiles.py
+++ MoveFiles.py
@@ -4,51 +4,34 @@ import time,os,json
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self,event):
-        folder_destination = { ".txt": (folder_to_track + "/txt") ,
-        ".pdf":(folder_to_track + "/PDF Documents") ,
-        ".zip":(folder_to_track + "/ZIP files"),
-        ".vsix":(folder_to_track + "/VS Code Extensions"),
-        ".png":(folder_to_track +"/Images"),
-        ".jpeg":(folder_to_track +"/Images"),
-        ".jpg":(folder_to_track +"/Images")}
-        for filename in os.listdir(folder_to_track):
-            src = folder_to_track + "/" + filename
-            if filename.endswith(".txt"):
-                new_dest = folder_destination[".txt"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".pdf"):
-                new_dest = folder_destination[".pdf"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".zip"):
-                new_dest = folder_destination[".zip"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".jpeg"):
-                new_dest = folder_destination[".jpeg"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".jpg"):
-                new_dest = folder_destination[".jpg"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".png"):
-                new_dest = folder_destination[".png"] + "/" + filename
-                os.rename(src,new_dest)
-            elif filename.endswith(".vsix"):
-                new_dest = folder_destination[".vsix"] + "/" + filename
-                os.rename(src,new_dest)
-        return True
+        # Reads the json file for address based on file extension
+        folder_destination = None
+        with open("data.json",'r') as file:
+            folder_destination = json.loads(file.read())
 
+        try:
+            for filename in os.listdir(folder_to_track):
+                try:
+                    name, ext = os.path.splitext(filename)
+                    new_dest = folder_destination[ext] + "/" + filename
+                    src = folder_to_track + "/" + filename
+                    os.rename(src,new_dest)
+                except KeyError:
+                    continue
+            return True
+        except:
+            return False
 
+folder_to_track = "/home/pramodkadam/Downloads"#address of folder to track
+
+eventHandler = MyHandler()
+observer = Observer()
+observer.schedule(eventHandler,folder_to_track,recursive=True)
+
+observer.start()
 try:
-    folder_to_track = input("Enter path of folder to track: ")
-    eventHandler = MyHandler()
-    observer = Observer()
-    observer.schedule(eventHandler,folder_to_track,recursive=True)
-
-    observer.start()
-    try:
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
-except:
-    print("Error")
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
