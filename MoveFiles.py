c090cf4fa81684e32bde5f53c8e9bdba0e158065
pramod21kadam <pramod21dkadam@gmail.com>
2020-08-24 16:56:28 +0530

    Added new file extensions and Improced code

--- MoveFiles.py
+++ MoveFiles.py
@@ -1,29 +1,43 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler 
-import time,os,json
+import time, os, json
 
 class MyHandler(FileSystemEventHandler):
-    def on_modified(self,event):
+    def on_created(self,event):
         # Reads the json file for address based on file extension
         folder_destination = None
-        with open("data.json",'r') as file:
-            folder_destination = json.loads(file.read())
-
+        file_size = -1
+        while file_size != os.path.getsize(event.src_path):
+            file_size = os.path.getsize(event.src_path)
+            time.sleep(5)
         try:
             for filename in os.listdir(folder_to_track):
                 try:
                     name, ext = os.path.splitext(filename)
-                    new_dest = folder_destination[ext] + "/" + filename
-                    src = folder_to_track + "/" + filename
-                    os.rename(src,new_dest)
-                except KeyError:
+                    found = False
+                    address = None
+                    for record in data_array:
+                        if ext in record["type"]:
+                            address = record["target_location"]
+                            new_dest = record["target_location"] + "/" + filename
+                            found = True
+                            break
+                    if found:
+                        src = folder_to_track + "/" + filename
+                        if not os.path.exists(address):
+                            
+                            os.mkdir(address)
+                        os.rename(src,new_dest)
+                except Exception as e:
+                    print(e)
                     continue
             return True
         except:
             return False
 
 folder_to_track = "/home/pramodkadam/Downloads"#address of folder to track
-
+with open("data.json",'r') as file:
+    data_array = json.loads(file.read())
 eventHandler = MyHandler()
 observer = Observer()
 observer.schedule(eventHandler,folder_to_track,recursive=True)
@@ -31,7 +45,7 @@ observer.schedule(eventHandler,folder_to_track,recursive=True)
 observer.start()
 try:
     while True:
-        time.sleep(10)
+        time.sleep(20)
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
